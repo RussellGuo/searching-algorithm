b3ac0a462ff906387a3d735a711eefb82a49c66d
Russell Guo <russell.grey@hotmail.com>
2018-08-26 19:11:44 +0800

    class figure added, it has attribute 'points' and 'lines'.
    .

--- figure.py
+++ figure.py
@@ -6,18 +6,71 @@ class Figure: pass
 
 
 class Figure:
-    def __init__(self, points, lines):
-        self.points = tuple(points)
-        self.lines = tuple(lines)
+    def __init__(self, parent: Figure, line: Line, new_point_checker=None):
+        self.parent = parent
+        self.new_point_checker = new_point_checker or parent.new_point_checker
+        parent_points, parent_lines = (parent.points, parent.lines) if parent else ((), ())
+        assert (line not in parent_lines)
+        self.lines = parent_lines + (line,)
+        new_points = []
+        for l in self.lines:
+            p = line.get_cross_point(l, self.new_point_checker)
+            if p and p not in parent_points and p not in new_points:
+                new_points.append(p)
+        self.points = tuple(sorted(list(parent_points) + new_points))
+        self.new_point_count = len(new_points)
+
+    def get_new_potential_lines(self):
+        points = tuple(sorted(self.points))
+        lines = []
+        for i in range(len(points)):
+            p1 = points[i]
+            for j in range(i +1, len(points)):
+                p2 = points[j]
+                l = Line.get_line_contains_points(p1, p2)
+                if l and l not in self.lines and l not in lines:
+                    lines.append(l)
+        return lines
+
+    def is_different_from_parent(self):
+        return self.new_point_count >= 1
 
     def __eq__(self, other: Figure):
-        return  self.points == other.points and self.lines == other.lines
+        return self.points == other.points and self.lines == other.lines
 
     def __hash__(self):
         return hash((self.points, self.lines))
 
+    def __repr__(self):
+        points = '\n'.join(str(i) for i in sorted(self.points))
+        lines = '\n'.join(str(i) for i in self.lines)
+
+        return '@\n' + points + '@\n#\n' + lines + '\n#'
+
+
 if __name__ == "__main__":
-    f1 = Figure([Point(fractions.Fraction(2, 3), fractions.Fraction(1, 1))], [])
-    f2 = Figure([Point(fractions.Fraction(4, 6), fractions.Fraction(1, 1))], [])
-    f=set([f1,f2])
-    print(f)
+    zero = fractions.Fraction(0)
+    one = fractions.Fraction(1)
+    max = fractions.Fraction(6)
+    init_figure = None
+
+
+    def my_new_point_checker(x: fractions.Fraction, y: fractions.Fraction):
+        return max >= x >= zero and max >= y >= zero
+
+
+    for i in range(7):
+        step = fractions.Fraction(i)
+        horn_line = Line(zero, one, step)
+        vert_line = Line(one, zero, step)
+        init_figure = Figure(init_figure, horn_line, my_new_point_checker)
+        init_figure = Figure(init_figure, vert_line, my_new_point_checker)
+    init_figure.parent = None
+    print(init_figure)
+    lines = (init_figure.get_new_potential_lines())
+    ff = set()
+    for l in lines:
+        f = Figure(init_figure, l)
+        if f.new_point_count > 0 and f not in ff:
+            ff.add(f)
+    ff = set()
--- geo.py
+++ geo.py
@@ -5,9 +5,9 @@ class Line: pass
 
 
 class Point:
-    def __init__(self, _x: fractions.Fraction, _y: fractions.Fraction):
-        self.x = _x
-        self.y = _y
+    def __init__(self, x: fractions.Fraction, y: fractions.Fraction):
+        self.x = x
+        self.y = y
 
     def __repr__(self):
         return '<%s, %s>' % (self.x, self.y)
@@ -16,7 +16,22 @@ class Point:
         return hash((self.x, self.y))
 
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+        return (self.y, self.x) == (other.y, other.x)
+
+    def __ne__(self, other):
+        return (self.y, self.x) != (other.y, other.x)
+
+    def __lt__(self, other):
+        return (self.y, self.x) < (other.y, other.x)
+
+    def __le__(self, other):
+        return (self.y, self.x) <= (other.y, other.x)
+
+    def __gt__(self, other):
+        return (self.y, self.x) > (other.y, other.x)
+
+    def __ge__(self, other):
+        return (self.y, self.x) >= (other.y, other.x)
 
 
 class Line:
@@ -33,27 +48,37 @@ class Line:
         c = (self.c * denominator).numerator
         g = math.gcd(a, b)
         g: int = math.gcd(g, c)
-        if a < 0 < g:  # let shadow_a be >= 0
-            g = -g
         self.__shadow_a = a // g
         self.__shadow_b = b // g
         self.__shadow_c = c // g
+        if self.__shadow_a < 0 or self.__shadow_a == 0 and self.__shadow_b < 0:
+            self.__shadow_a *= -1
+            self.__shadow_b *= -1
+            self.__shadow_a *= -1
 
     def contain_point(self, p: Point) -> bool:
         l = self.__shadow_a * p.x + self.__shadow_b * p.y - self.__shadow_c
         return l == 0
 
-    def get_cross_point(self, other: Line) -> Point:
+    def get_cross_point(self, other: Line, new_point_checker=None) -> Point:
         def det2(_a, _b, _c, _d):
             return _a * _d - _b * _c
 
         _d = det2(self.__shadow_a, self.__shadow_b, other.__shadow_a, other.__shadow_b)
+        if _d == 0:
+            return None
+
         dx = det2(self.__shadow_c, self.__shadow_b, other.__shadow_c, other.__shadow_b)
         dy = det2(self.__shadow_a, self.__shadow_c, other.__shadow_a, other.__shadow_c)
-        return Point(fractions.Fraction(dx, _d), fractions.Fraction(dy, _d))
+        x, y = fractions.Fraction(dx, _d), fractions.Fraction(dy, _d)
+        if new_point_checker and not new_point_checker(x, y):
+            return None
+        return Point(x, y)
 
     @staticmethod
     def get_line_contains_points(p1: Point, p2: Point) -> Line:
+        if p1 == p2:
+            return None
         a = p1.y - p2.y
         b = p2.x - p1.x
         c = p2.x * p1.y - p1.x * p2.y
@@ -61,7 +86,7 @@ class Line:
         return line
 
     def __repr__(self):
-        return '<%s * x + %s * y = %s>' % (self.a, self.b, self.c)
+        return '<%s * x + %s * y = %s>' % (self.__shadow_a, self.__shadow_b, self.__shadow_c)
 
     def __hash__(self):
         return hash((self.__shadow_a, self.__shadow_b, self.__shadow_c))
@@ -81,13 +106,14 @@ if __name__ == "__main__":
 
     x3 = x4 = x2 + 4
     print((x1 + x2 + x3 + x4) / 4)
-    p1 = Point(0, 0)
-    p2 = Point(2, 2)
-    p3 = Point(1, 1.5)
-    p4 = Point(1.5, 0.5)
-    l1 = Line.get_line_contains_points(p1, p2)
-    l2 = Line.get_line_contains_points(p3, p4)
-    l3 = Line.get_line_contains_points(p1, p2)
-    print(set([l3, l1]))
-    p = l1.get_cross_point(l2)
+    test_p1 = Point(0, 0)
+    test_p2 = Point(2, 2)
+    test_p3 = Point(1, 1.5)
+    test_p4 = Point(1.5, 0.5)
+    test_l1 = Line.get_line_contains_points(test_p1, test_p2)
+    test_l2 = Line.get_line_contains_points(test_p3, test_p4)
+    test_l3 = Line.get_line_contains_points(test_p1, test_p2)
+    print({test_l3, test_l1})
+    p = test_l1.get_cross_point(test_l2)
     print(p)
+    print(test_l1.get_cross_point(test_l1))
