95932dba434c95d05331e058985aa1754df6904d
Russell Guo <russell.grey@hotmail.com>
2018-09-01 15:51:59 +0800

    print graph.
    .

--- geo_item_dumper.py
+++ geo_item_dumper.py
@@ -1,5 +1,5 @@
 from fractions import Fraction
-from typing import FrozenSet
+from typing import FrozenSet, Tuple
 
 from figure import Figure
 from geo import Point, Line
@@ -7,35 +7,58 @@ from geo import Point, Line
 
 class GeoItemDumperBase:
     def __init__(self):
-        pass
-
-    def new_root(self, fig_root: Figure):
-        pass
-
-    def new_figure(self, f: Figure, l: Line, new_points: FrozenSet[Point]):
-        pass
-
-
-class GeoItemDumperConsole:
-    def __init__(self):
-        super(GeoItemDumperConsole, self).__init__()
         self.figure_count = self.line_count = self.point_count = 0
-
-    def new_root(self, fig: Figure):
-        self.figure_count += 1
-        self.line_count += len(fig.lines)
-        self.point_count += len(fig.new_points) + len(fig.base_points)
         pass
 
-    def new_figure(self, f: Figure, l: Line, new_points: FrozenSet[Point]):
-        self.figure_count += 1
-        self.line_count += 1
-        self.point_count += len(new_points)
+    def open(self):
         pass
 
     def close(self):
-        print(self.point_count, self.line_count, self.figure_count)
         pass
+
+    def output_figure(self, values: Tuple[int, int, int]):
+        pass
+
+    def output_line(self, values: Tuple[int, int, int, int, int, int]):
+        pass
+
+    def output_point(self, values: Tuple[int, int, int, int, int, int, int]):
+        pass
+
+    def new_root(self, fig_root: Figure):
+        self.figure_count += 1
+        self.line_count += len(fig_root.lines)
+        self.point_count += len(fig_root.new_points) + len(fig_root.base_points)
+        self.output_figure((fig_root.id, 0, 0))
+        for line in fig_root.lines:
+            self.output_line((line.id, line.a, line.b, line.c, 0, 0))
+        for p in fig_root.new_points | fig_root.base_points:
+            self.output_point((p.id, p.x.numerator, p.x.denominator, p.y.numerator, p.y.denominator, 0, 0))
+
+    def new_figure(self, f: Figure, line: Line, new_points: FrozenSet[Point]):
+        self.figure_count += 1
+        self.line_count += 1
+        self.point_count += len(new_points)
+        self.output_figure((f.id, f.parent.id, line.id))
+        self.output_line((line.id, line.a, line.b, line.c, line.obj_tuple[1].id, line.obj_tuple[2].id))
+        for p in new_points:
+            self.output_point((p.id, p.x.numerator, p.x.denominator, p.y.numerator, p.y.denominator, p.obj_tuple[1].id,
+                               p.obj_tuple[2].id))
+
+
+class GeoItemDumperConsole(GeoItemDumperBase):
+
+    def output_figure(self, values: Tuple[int, int, int]):
+        print(values)
+
+    def output_line(self, values: Tuple[int, int, int, int, int, int]):
+        print(values)
+
+    def output_point(self, values: Tuple[int, int, int, int, int, int, int]):
+        print(values)
+
+    def close(self):
+        print(self.point_count, self.line_count, self.figure_count)
 
 
 def dump_every_graph():
@@ -50,12 +73,11 @@ def dump_every_graph():
     # try to find it
     point_target = Point(Fraction(-1), Fraction(0), "UNTOUCHABLE")
     dumper = GeoItemDumperConsole()
-    result = search(exam_figure, point_target, 2, dumper)
+    dumper.open()
+    search(exam_figure, point_target, 3, dumper)
     dumper.close()
-
-    return result
 
 
 if __name__ == "__main__":
-    fig, point = dump_every_graph()
+    dump_every_graph()
     print(Point.id, Line.id, Figure.id)
