7d5708dfe137ccc8542e6f11314741350d6551e3
Russell Guo <russell.grey@hotmail.com>
2018-12-14 20:49:08 +0800

    optimized speed. verified output by find different with Python's version. It's OK now.
    .

--- bfs.cpp
+++ bfs.cpp
@@ -1,5 +1,6 @@
 #include <set>
 #include <map>
+#include <forward_list>
 #include <unordered_map>
 
 #include <boost/range/join.hpp>
@@ -9,11 +10,27 @@
 #include "figure.h"
 using namespace geo;
 
+inline static void output_point_figure(const Figure::FigurePtr &fig_ptr, const PointSet& points)
+{
+    if (points.size() <= 0) {
+        return;
+    }
+    for (const auto &line: fig_ptr->getLines()) {
+        Int a, b, c;
+        line.getParams(a, b, c);
+        fprintf(stderr, "%d %d %d ", a, b, c);
+    }
+    fprintf(stderr, "0 0 0 ");
+
+    for (const auto &point: points) {
+        auto x(point.getX()), y(point.getY());
+        fprintf(stderr, "%d %d %d %d ", x.numerator(), x.denominator(), y.numerator(), y.denominator());
+    }
+    fprintf(stderr, "0 0 0 0\n");
+}
+
 bfs::bfs(const unsigned max_level)
 {
-    typedef std::vector<Figure::FigurePtr> FigList;
-    typedef std::map<Point, FigList> MapForPointToFig;
-
     const auto begin_time = boost::posix_time::microsec_clock::local_time();
 
     LineSet init_lines;
@@ -49,40 +66,29 @@ bfs::bfs(const unsigned max_level)
     }
 
     Figure *init_fig = new Figure();
-    FigSet fig_tab;
-    fig_tab.insert(init_fig);
+    FigSet cur_level_fig_tab;
+    cur_level_fig_tab.insert(init_fig);
+    output_point_figure(init_fig, init_points);
 
-    MapForPointToFig point_fig_map;
-    for (const auto &p: init_points) {
-        auto& fig_list = point_fig_map[p];
-        fig_list.push_back(init_fig);
-    }
+    PointSet low_level_point_set(init_points);
 
     size_t count = 0;
 
     for (size_t level = 0; level <= max_level; level++) {
-        std::vector<const Figure*> cur_level_fig_tab;
-        for (auto fig:fig_tab) {
-            if (fig->lineCount() == level) {
-                cur_level_fig_tab.push_back(fig);
-            }
-        }
+        PointSet cur_level_point_set;
+        FigSet next_level_fig_tab;
         printf("level: %zd, total figures %zd\n", level, cur_level_fig_tab.size());
-        if (level == max_level) {
-        }
-        for (const auto cur_fig:cur_level_fig_tab) {
+        for(const auto cur_fig:cur_level_fig_tab) {
             if (count % 10000 == 0) {
                 const auto microsec = boost::posix_time::microsec_clock::local_time() - begin_time;
-                printf("log:%10zd, ", count);
-                printf("%10zd, %10zd, %10ld\n",
-                       cur_level_fig_tab.size(),
-                       point_fig_map.size(), microsec.total_microseconds());
+                printf("log:%10zd, %10ld\n", count, microsec.total_microseconds());
                 fflush(stdout);
             }
             count++;
             // find all the point and record them
             // should use all lines cross (all lines + init_lines)
-            PointSet new_points;
+            PointSet cur_fig_points_other_than_init_fig;
+            PointSet cur_fig_points_for_cur_level;
 
             const auto &line_vector(cur_fig->getLines());
             LineSet geo_lines(line_vector.cbegin(), line_vector.cend());
@@ -97,26 +103,27 @@ bfs::bfs(const unsigned max_level)
                     if (!Line::getIntersectionPoint(line_a, line_b, point)) {
                         continue;
                     }
-                    auto ret = new_points.insert(point);
-                    if (ret.second) {
-                        auto &fig_lst = point_fig_map[point];
-                        if (fig_lst.empty() || fig_lst[0]->lineCount() >= level ) {
-                            fig_lst.push_back(cur_fig);
-                        }
+                    auto inserted = cur_fig_points_other_than_init_fig.insert(point).second;
+                    if (inserted && low_level_point_set.find(point) == low_level_point_set.end()) {
+                        cur_fig_points_for_cur_level.insert(point);
                     }
                 }
             }
+            output_point_figure(cur_fig, cur_fig_points_for_cur_level);
 
             if (level == max_level) {
                 continue;
             }
+            for (const auto &p:cur_fig_points_for_cur_level) {
+                cur_level_point_set.insert(p);
+            }
             LineSet new_lines(first_level_line_set);
             {
                 PointSet all_points(init_points);
-                for (const auto &point:new_points) {
+                for (const auto &point:cur_fig_points_other_than_init_fig) {
                     all_points.insert(point);
                 }
-                for (const auto& point_1:new_points) {
+                for (const auto& point_1:cur_fig_points_other_than_init_fig) {
                     for(const auto& point_2: all_points) {
                         Line line(point_1, point_2);
                         if (!line.isValid()) {
@@ -131,29 +138,23 @@ bfs::bfs(const unsigned max_level)
                     continue;
                 }
                 Figure fig(*cur_fig, line);
-                if (fig_tab.find(&fig) == fig_tab.cend()) {
+                if (next_level_fig_tab.find(&fig) == next_level_fig_tab.cend()) {
                     Figure *ptr = new Figure(fig);
-                    fig_tab.insert(ptr);
+                    next_level_fig_tab.insert(ptr);
                 }
             }
 
             cur_fig->getLines();
         }
-    }
-
-    printf("total:%10zd, %10zd\n", fig_tab.size(), point_fig_map.size());
-    std::vector<Figure::FigurePtr>fig_vec(fig_tab.cbegin(), fig_tab.cend());
-    Figure::pointLess comp;
-    std::sort(fig_vec.begin(), fig_vec.end(), comp);
-    for(const auto& f:fig_vec) {
-        const auto& lines = f->getLines();
-        for (const auto &line: lines) {
-            Int a, b, c;
-            line.getParams(a, b, c);
-            printf("%d %d %d  ", a, b, c);
+        {
+            swap(low_level_point_set, cur_level_point_set);
+            for (const auto &p:cur_level_point_set) {
+                low_level_point_set.insert(p);
+            }
         }
-        printf("\n");
-    }
+        cur_level_fig_tab.clear();
+        swap(cur_level_fig_tab, next_level_fig_tab);
 
+    }
 }
 
