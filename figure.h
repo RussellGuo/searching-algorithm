7e196c5e07bbc67d14f1263c4a8bd8a561f8f762
Russell Guo <russell.grey@hotmail.com>
2018-12-18 18:45:49 +0800

    simplified class figure.
    .

--- figure.h
+++ figure.h
@@ -5,58 +5,56 @@
 #include <vector>
 
 namespace geo {
-class Figure
+class Figure: private std::vector<Line>
 {
 public:
-    Figure():lines() {}
-    Figure(const Figure &parent, const Line &ll):lines(parent.lines) {
-        lines.push_back(ll);
-        std::sort(lines.begin(), lines.end());
-        lines.shrink_to_fit();
+    Figure() {}
+    Figure(const Figure &parent, const Line &ll):vector(parent) {
+        push_back(ll);
+        std::sort(begin(), end());
+        shrink_to_fit();
     }
 
     const std::vector<Line> &getLines() const {
-        return lines;
+        return *this;
     }
     size_t lineCount() const {
-        auto ret = lines.size();
+        auto ret = size();
         return ret;
     }
 
     void setEightSymmetry(Figure symmetryArray[8]) const {
         uint16_t line_count = uint16_t(lineCount());
         for (uint16_t i = 0; i < 8; i++) {
-            symmetryArray[i].lines.resize(line_count);
+            symmetryArray[i].resize(line_count);
         }
         Line LineArray[8];
         for (uint16_t ll = 0; ll < line_count; ll++) {
-            lines[ll].setEightSymmetry(LineArray);
+            (*this)[ll].setEightSymmetry(LineArray);
             for (uint16_t i = 0; i < 8; i++) {
-                symmetryArray[i].lines[ll] =LineArray[i];
+                symmetryArray[i][ll] =LineArray[i];
             }
         }
         for (uint16_t i = 0; i < 8; i++) {
-            std::sort(symmetryArray[i].lines.begin(), symmetryArray[i].lines.end());
+            std::sort(symmetryArray[i].begin(), symmetryArray[i].end());
         }
     }
 
-    Figure(const Figure &fig):lines(fig.lines) {}
+    Figure(const Figure &fig) = default;
     Figure& operator =(const Figure &) = delete;
 
-    bool operator ==(const Figure &other) const { return lines == other.lines;}
+    bool operator ==(const Figure &other) const { return vector(*this) == other;}
     bool operator <(const Figure &other) const {
         if (lineCount() == other.lineCount()) {
             for (uint16_t i = 0; i < lineCount(); i++) {
-                if (lines[i] != other.lines[i]) {
-                    return lines[i] < other.lines[i];
+                if ((*this)[i] != other[i]) {
+                    return (*this)[i] < other[i];
                 }
             }
             return false;
         } else {
             return lineCount() < other.lineCount();
         }
-
-
     }
 
     struct hash {
@@ -64,7 +62,7 @@ public:
             size_t ret;
             ret = 0;
             Line::hash hash;
-            for (auto line:fig.lines) {
+            for (auto line:vector(fig)) {
                 auto a = hash(line);
                 ret *= 97;
                 ret += a;
@@ -73,8 +71,6 @@ public:
         }
     };
 
-private:
-    std::vector<Line> lines;
 };
 
 typedef std::set<Figure> FigSet;
